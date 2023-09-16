a739780330e7dc3f489ec0b53abee745e390fdd2
sruthina2002 <89062755+sruthina2002@users.noreply.github.com>
2023-09-17 00:14:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+window.addEventListener("load",checkInternetConnection);
+
+
+function checkInternetConnection(){
+
+    const statusText = document.getElementById('statusText');
+    const ipAddressText = document.getElementById('ipAddressText');
+    const networkStrengthText = document.getElementById('networkStrengthText');
+
+    statusText.textContent = 'Checking....';
+
+    if(navigator.online){
+        fetch( 'https://api.ipify.org?format=json')
+        .then((response)=> response.json())
+        .then((data)=>{
+
+            ipAddressText.textContent = data.ip;
+            statusText.textContent = 'Connected'
+
+            const connection = navigator = connection ?connection.downlink +'Mbps' : 'Unkown';
+            networkStrengthText.textContent = networkStrength;
+
+
+        })
+        .catch(()=>{
+            statusText.textContent='Disconnected';
+            ipAddressText.textContent= '-'
+            networkStrengthText.textContent= '-'
+        })
+
+    }else{
+        statusText.textContent='Disconnected';
+        ipAddressText.textContent= '-'
+        networkStrengthText.textContent= '-'
+    }
+}
